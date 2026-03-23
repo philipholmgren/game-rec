dda61b7bb4f0f1a83ea0a465350c734e52d73cc9
Philip Holmgren <philip.holmgren@live.com>
2026-03-23 22:33:56 +0100

    new path

--- src/http/getRecommendations.tsx
+++ src/http/getRecommendations.tsx
@@ -1,7 +1,7 @@
 import type { RecommendationRequest } from "../types/commonTypes";
 
 export async function getRecommendations(payload: RecommendationRequest) {
-  const response = await fetch("/api/recommendations", {
+  const response = await fetch("https://api.nextgame.se/recommendations", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
